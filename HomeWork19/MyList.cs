5b13f2923035c3b75ed51209b2b33fd5dc3ab45a
Tsimafei Shvedau <tsimafeishvedau@gmail.com>
2022-05-12 14:06:24 +0300

    Добавьте файлы проекта.

---
+++ HomeWork19/MyList.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork19
+{
+    internal class MyList<T> : IList<T> where T : IComparable<T>
+    {
+        
+        private T[] _list = new T[1];
+        public int Count { get; private set; }
+        public int Capacity => _list.Length;
+        public T this[int index]
+        {
+            get { return _list[index]; }
+            set => _list[index] = value;
+        }
+
+        private void TryToIncreaseInternalArray()
+        {
+            if (Count == Capacity)
+            {
+                T[] tmp = new T[Capacity * 2];
+                _list.CopyTo(tmp, 0);
+                _list = tmp;
+            }
+        }
+        public bool IsReadOnly { get; }
+
+        public void Add(T elem)
+        {
+            TryToIncreaseInternalArray();
+            _list[Count] = elem;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            _list = new T[1];
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return _list.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            int tmp = 0;
+            for (int i = arrayIndex; i < array.Length; i++)
+            {
+                array[i] = _list[tmp];
+                tmp++;
+            }
+        }
+
+
+        public int IndexOf(T item) 
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_list[i].CompareTo(item) == 0) return i;
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < Count && index >= 0)
+            {
+                Count++;
+                TryToIncreaseInternalArray();
+                for (int i = Count-1; i > index; i--)
+                {
+                    _list[i] = _list[i-1];
+                }
+                _list[index] = item;
+            }
+            else throw new IndexOutOfRangeException("Неверный индекс нового элемента");
+        }
+
+        public void PrintListToConsole()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(_list[i]);
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            if(_list.Contains(item))
+            {
+                for (int i = Array.IndexOf(_list, item); i < Count; i++)
+                {
+                    _list[i] = _list[i + 1];
+                }
+                Count--;
+                return true;
+            };
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < Count && index >= 0)
+            {
+                for (int i = index; i < Count; i++)
+                {
+                    _list[i] = _list[i + 1];
+                }
+                Count--;
+            }
+            else throw new IndexOutOfRangeException("Неверный индекс");
+        }
+
+        public T[] Get_list()
+        {
+            return _list;
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new MyListEnumerator(Get_list());
+        }
+
+        public MyList ()
+        {
+            _list = new T[1];
+            Count = 0;
+        }
+
+        public MyList (int count)
+        {
+            _list = new T[count];
+            Count = 0;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ HomeWork19/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork19
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> lst = new MyList<int>(100);
+
+            lst.Add(1);
+
+            lst.Add(2);
+
+
+            lst.Add(3);
+            lst.Add(4);
+            lst.Add(5);
+            lst.Add(6);
+            lst.Add(7);
+            lst.Add(8);
+                lst.Add(9);
+
+            lst.Insert(0, 8);
+
+            lst.Remove(1);
+
+            lst.PrintListToConsole();
+            //int[] array = new int[5];
+            //lst.CopyTo(array, 0);
+            Console.WriteLine("_____________________________________-");
+            Console.WriteLine(lst.Capacity);
+            lst.Clear();
+            Console.WriteLine(lst.Capacity);
+
+
+
+            Console.WriteLine("_____________________________________-");
+
+            foreach(var elem in lst)
+            {
+                Console.WriteLine(elem);
+            }
+
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+
+            //Console.WriteLine(lst.IndexOf(25));
+
+            List<int> vs = new List<int>();
+            
+            Console.ReadKey();
+        }
+    }
+}
